15a3c68ae758e1b3cf263a91b598739acd19c5db
Anushka Dilshan <anushkadilshan990@gmail.com>
2025-01-29 14:59:47 +0530

    update code add

---
+++ Script/animation1.js
@@ -0,0 +1,14 @@
+
+ScrollReveal({ 
+//reset: true,
+distance: '60px',
+duration:2500,
+delay:400
+});
+
+ScrollReveal().reveal('.gold-line, .expert-sub-title, .expert-details, .footer-logo', { delay: 500, origin:'left' });
+ScrollReveal().reveal('.salon-name, .cta-button', { delay: 600, origin:'bottom' });
+ScrollReveal().reveal('.stylist-photo', { delay: 700, origin:'right' });
+ScrollReveal().reveal('.faq-question, .footer-links', { delay: 500, origin:'bottom',interval: 200 });
+ScrollReveal().reveal('.expert-title, .faq-title', { delay: 500, origin:'top' });
+ScrollReveal().reveal('.tagline, .footer-contact', { delay: 500, origin:'right',interval: 200 });
